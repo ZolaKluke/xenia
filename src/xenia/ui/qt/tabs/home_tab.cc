65e7b21d3766ba0dac1c2ca21040ffc5772438f8
Satori <comfycallum@gmail.com>
2018-11-29 16:40:27 +0000

    Add separator to sidebar title

--- src/xenia/ui/qt/tabs/home_tab.cc
+++ src/xenia/ui/qt/tabs/home_tab.cc
@@ -32,21 +32,29 @@ void HomeTab::BuildSidebar() {
 
   QVBoxLayout* title_layout = new QVBoxLayout;
   title_layout->setMargin(0);
-  title_layout->setContentsMargins(0, 0, 0, 0);
+  title_layout->setContentsMargins(0, 40, 0, 0);
   title_layout->setSpacing(0);
 
   sidebar_title->setLayout(title_layout);
 
+  // Title labels
   QLabel* xenia_title = new QLabel("Xenia");
   xenia_title->setObjectName("titleLabel");
 
   QLabel* xenia_subtitle = new QLabel("Xbox 360 Emulator");
   xenia_subtitle->setObjectName("subtitleLabel");
 
-  title_layout->addWidget(xenia_title, 0, Qt::AlignCenter | Qt::AlignBottom);
-  title_layout->addWidget(xenia_subtitle, 0, Qt::AlignCenter | Qt::AlignTop);
+  title_layout->addWidget(xenia_title, 0, Qt::AlignHCenter | Qt::AlignBottom);
+  title_layout->addWidget(xenia_subtitle, 0, Qt::AlignHCenter | Qt::AlignTop);
 
-  sidebar_layout->addWidget(sidebar_title, 0, Qt::AlignTop);
+  // Title separator
+  QWidget* separator = new QWidget;
+  separator->setObjectName("separator");
+
+  title_layout->addWidget(separator);
+
+  // Add title components to sidebar
+  sidebar_layout->addWidget(sidebar_title, 0, Qt::AlignHCenter | Qt::AlignTop);
 
   // Add sidebar to tab widget
   layout_->addWidget(sidebar, 0, Qt::AlignLeft);
