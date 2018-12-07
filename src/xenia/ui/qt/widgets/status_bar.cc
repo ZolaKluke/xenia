95f53e5eea29dad4636dd74118225bfff82a27ab
Satori <comfycallum@gmail.com>
2018-12-07 20:40:36 +0000

    Move status bar styles to css

--- src/xenia/ui/qt/widgets/status_bar.cc
+++ src/xenia/ui/qt/widgets/status_bar.cc
@@ -6,17 +6,12 @@ namespace qt {
 
 XStatusBar::XStatusBar(QWidget* parent)
     : Themeable<QStatusBar>("XStatusBar", parent) {
-  // TODO: move to css
-  setStyleSheet(
-      "background: rgb(40, 40, 40);"
-      "color: #c7c7c7;");
-
   setSizeGripEnabled(false);
 
   QGraphicsDropShadowEffect* effect = new QGraphicsDropShadowEffect;
   effect->setBlurRadius(16);
   effect->setXOffset(0);
-  effect->setYOffset(-4);
+  effect->setYOffset(-2);
   effect->setColor(QColor(0, 0, 0, 64));
 
   setGraphicsEffect(effect);
