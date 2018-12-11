fa873e7b14a8cf312b23f6b86cf694c18bc2aa94
Satori <comfycallum@gmail.com>
2018-12-11 19:06:10 +0000

    Improve toolbar glyph quality

--- src/xenia/ui/qt/actions/action.cc
+++ src/xenia/ui/qt/actions/action.cc
@@ -9,7 +9,7 @@ namespace qt {
 XAction::XAction() {}
 
 XAction::XAction(const QChar& icon, const QString& text) {
-  setGlyphIcon(QFont("Segoe MDL2 Assets", 32), icon);
+  setGlyphIcon(QFont("Segoe MDL2 Assets", 64), icon);
   setIconText(text);
 }
 
@@ -24,25 +24,28 @@ void XAction::rebuildGlyphIcons() {
   auto renderPixmap = [=](QColor color) {
     // Measure the Glyph
     QFontMetrics measure(glyph_font_);
-    QRectF icon_rect = measure.boundingRect(glyph_char_);
+    QRect icon_rect = measure.boundingRect(glyph_char_);
     double max = qMax(icon_rect.width(), icon_rect.height());
+
     // Create the Pixmap
-    QPixmap pixmap(max, max);
+    // boundingRect can be inaccurate so add a 4px padding to be safe
+    QPixmap pixmap(max + 2, max + 2);
     pixmap.fill(Qt::transparent);
 
     // Paint the Glyph
     QPainter painter(&pixmap);
-    painter.setRenderHints(QPainter::Antialiasing | QPainter::TextAntialiasing);
     painter.setFont(glyph_font_);
-    painter.setPen(QPen(Qt::white));
+    painter.setRenderHints(QPainter::Antialiasing | QPainter::TextAntialiasing);
+    painter.setPen(QPen(color));
 
     painter.drawText(pixmap.rect(), Qt::AlignVCenter, glyph_char_);
 
     return pixmap;
   };
 
-  QPixmap OFF = renderPixmap(glyph_palette_.color(QPalette::Foreground));
-  QPixmap ON = renderPixmap(glyph_palette_.color(QPalette::Highlight));
+  // TODO: load colors from CSS
+  QPixmap OFF = renderPixmap(Qt::white);
+  QPixmap ON = renderPixmap(Qt::white);
 
   QIcon icon;
   icon.addPixmap(OFF, QIcon::Normal, QIcon::Off);
