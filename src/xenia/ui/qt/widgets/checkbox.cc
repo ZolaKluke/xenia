673b3f8a5a86e7f73dbbb17ce72a5fb93000c1b1
Satori <comfycallum@gmail.com>
2019-02-04 14:21:25 +0000

    Add getters/setters, theme coloring and comments to XCheckBox

--- src/xenia/ui/qt/widgets/checkbox.cc
+++ src/xenia/ui/qt/widgets/checkbox.cc
@@ -1,33 +1,48 @@
 #include "checkbox.h"
 #include <QStyleOption>
+#include "xenia/ui/qt/theme_manager.h"
 
 namespace xe {
 namespace ui {
 namespace qt {
 
 XCheckBox::XCheckBox(QWidget* parent)
-    : Themeable<QCheckBox>("XCheckBox", parent) {}
+    : Themeable<QCheckBox>("XCheckBox", parent) {
+  Update();
+}
+
+void XCheckBox::Update() {
+  // Load colors from the current theme file.
+  // Loading colors from the theme directly is not an ideal situation as the
+  // component will ignore CSS theming.
+  // This should also be updated in paintEvent() as a theme change would not be
+  // updated for this component otherwise.
+  const Theme& theme = ThemeManager::Instance().current_theme();
+  border_color_ = theme.ColorForKey("light2");
+  checked_color_ = theme.ColorForKey("secondary");
+}
 
 void XCheckBox::paintEvent(QPaintEvent* e) {
+  Update();
+
   QStyleOptionButton option;
   initStyleOption(&option);
 
-  QRect indicator_rect = style()->proxy()->subElementRect(
-      QStyle::SE_CheckBoxIndicator, &option, this);
+  // get original rect for checkbox label
 
   QRect label_rect = style()->proxy()->subElementRect(
       QStyle::SE_CheckBoxContents, &option, this);
+  label_rect.translate(label_indent_, 0);
 
-  label_rect.translate(8, 0);
-
+  // create rect for indicator box
+  // rect must start at 1 as the painter draws either side of start offset so
+  // starting at (0,0) would leave 2 sides cut off
   QRectF indicator_box = QRectF(1, 1, 16, 16);
 
   QPainter painter(this);
   painter.setRenderHints(QPainter::Antialiasing);
 
-  // TODO: remove hardcoded colors
-
-  QPen pen(QColor(196, 196, 196));
+  QPen pen(border_color_);
   pen.setJoinStyle(Qt::MiterJoin);
   painter.setPen(pen);
 
@@ -35,9 +50,10 @@ void XCheckBox::paintEvent(QPaintEvent* e) {
 
   painter.drawText(label_rect, text());
 
+  // paint checked inner box if checkbox is checked
   if (isChecked()) {
     painter.setPen(Qt::transparent);
-    QBrush checked_brush = QBrush(QColor(92, 228, 148));
+    QBrush checked_brush = QBrush(checked_color_);
     QRectF checked_rect = QRectF(3, 3, 12, 12);
 
     painter.setBrush(checked_brush);
@@ -46,11 +62,11 @@ void XCheckBox::paintEvent(QPaintEvent* e) {
 }
 
 QSize XCheckBox::sizeHint() const {
-  // Increase sizeHint by 8 to compensate for slightly larget indicator box and
-  // translated label.
-  // This is not exact, but to get it exact would require using
-  // an algorithm with QFontMetrics.
-  return QCheckBox::sizeHint() + QSize(8, 0);
+  // Increase sizeHint by indent amount to compensate for slightly larget
+  // indicator box and translated label.
+  // This is not exact, but to get it exact would require using an algorithm
+  // with QFontMetrics.
+  return QCheckBox::sizeHint() + QSize(label_indent_, 0);
 }
 
 }  // namespace qt
--- src/xenia/ui/qt/widgets/checkbox.h
+++ src/xenia/ui/qt/widgets/checkbox.h
@@ -13,11 +13,24 @@ class XCheckBox : public Themeable<QCheckBox> {
  public:
   explicit XCheckBox(QWidget* parent = nullptr);
 
+  double label_indent() const { return label_indent_; }
+  void set_label_indent(double indent) { label_indent_ = indent; }
+
+  const QColor& border_color() const { return border_color_; }
+  void set_border_color(const QColor& color) { border_color_ = color; }
+  const QColor& checked_color() const { return checked_color_; }
+  void set_checked_color(const QColor& color) { checked_color_ = color; }
+
  protected:
   void paintEvent(QPaintEvent* e) override;
   QSize sizeHint() const override;
 
  private:
+  void Update();
+
+  double label_indent_ = 8.0;
+  QColor border_color_;
+  QColor checked_color_;
 };
 
 }  // namespace qt
