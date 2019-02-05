5622190d84925832a5f0387518bb700adca3e5da
Satori <comfycallum@gmail.com>
2019-02-05 21:17:47 +0000

    Add XRadioButton component

--- src/xenia/ui/qt/widgets/radio_button.cc
+++ src/xenia/ui/qt/widgets/radio_button.cc
@@ -1,1 +1,79 @@
 #include "radio_button.h"
+#include <QStyleOption>
+#include "xenia/ui/qt/theme_manager.h"
+
+namespace xe {
+namespace ui {
+namespace qt {
+
+XRadioButton::XRadioButton(QWidget* parent)
+    : Themeable<QRadioButton>("XRadioButton", parent) {
+  Update();
+}
+
+void XRadioButton::Update() {
+  // Load colors from the current theme file.
+  // Loading colors from the theme directly is not an ideal situation as the
+  // component will ignore CSS theming.
+  // This should also be updated in paintEvent() as a theme change would not be
+  // updated for this component otherwise.
+  const Theme& theme = ThemeManager::Instance().current_theme();
+
+  if (!border_color_.isValid()) {
+    border_color_ = theme.ColorForKey("light2");
+  }
+  if (!checked_color_.isValid()) {
+    checked_color_ = theme.ColorForKey("secondary");
+  }
+}
+
+void XRadioButton::paintEvent(QPaintEvent* e) {
+  Update();
+
+  QStyleOptionButton option;
+  initStyleOption(&option);
+
+  // get original rect for radio button label
+
+  QRect label_rect = style()->proxy()->subElementRect(
+      QStyle::SE_RadioButtonContents, &option, this);
+  label_rect.translate(label_indent_, 0);
+
+  // create rect for indicator box
+  // rect must start at 1 as the painter draws either side of start offset so
+  // starting at (0,0) would leave 2 sides cut off
+  QRectF indicator_box = QRectF(1, 1, 16, 16);
+
+  QPainter painter(this);
+  painter.setRenderHints(QPainter::Antialiasing);
+
+  QPen pen(border_color_);
+  pen.setJoinStyle(Qt::MiterJoin);
+  painter.setPen(pen);
+
+  painter.drawEllipse(indicator_box.center(), 8, 8);
+
+  painter.drawText(label_rect, text());
+
+  // paint checked inner box if radio button is checked
+  if (isChecked()) {
+    painter.setPen(Qt::transparent);
+    QBrush checked_brush = QBrush(checked_color_);
+    QRectF checked_rect = QRectF(3, 3, 12, 12);
+
+    painter.setBrush(checked_brush);
+    painter.drawEllipse(checked_rect.center(), 6, 6);
+  }
+}
+
+QSize XRadioButton::sizeHint() const {
+  // Increase sizeHint by indent amount to compensate for slightly larget
+  // indicator box and translated label.
+  // This is not exact, but to get it exact would require using an algorithm
+  // with QFontMetrics.
+  return QRadioButton::sizeHint() + QSize(label_indent_, 0);
+}
+
+}  // namespace qt
+}  // namespace ui
+}  // namespace xe
--- src/xenia/ui/qt/widgets/radio_button.h
+++ src/xenia/ui/qt/widgets/radio_button.h
@@ -1,4 +1,40 @@
 #ifndef XENIA_UI_QT_RADIO_BUTTON_H_
 #define XENIA_UI_QT_RADIO_BUTTON_H_
 
+#include <QRadioButton>
+#include "xenia/ui/qt/themeable_widget.h"
+
+namespace xe {
+namespace ui {
+namespace qt {
+
+class XRadioButton : public Themeable<QRadioButton> {
+  Q_OBJECT
+ public:
+  explicit XRadioButton(QWidget* parent = nullptr);
+
+  double label_indent() const { return label_indent_; }
+  void set_label_indent(double indent) { label_indent_ = indent; }
+
+  const QColor& border_color() const { return border_color_; }
+  void set_border_color(const QColor& color) { border_color_ = color; }
+  const QColor& checked_color() const { return checked_color_; }
+  void set_checked_color(const QColor& color) { checked_color_ = color; }
+
+ protected:
+  void paintEvent(QPaintEvent* e) override;
+  QSize sizeHint() const override;
+
+ private:
+  void Update();
+
+  double label_indent_ = 8.0;
+  QColor border_color_;
+  QColor checked_color_;
+};
+
+}  // namespace qt
+}  // namespace ui
+}  // namespace xe
+
 #endif
